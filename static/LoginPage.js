41a7c5dca15fb85108933bbf41ab0cd4d204f564
s2356546 <f.gonzalez-novolopez@student.utwente.nl>
2022-02-22 15:38:35 +0100

    Front-end

--- static/LoginPage.js
+++ static/LoginPage.js
@@ -2,3 +2,7 @@
 function close(){
     window.location.href = 'MainPage.html';
 };
+
+document.getElementById("close").addEventListener("click", function() {
+    window.location.href = 'MainPage.html';
+});
--- static/MainPage.js
+++ static/MainPage.js
@@ -10,3 +10,5 @@ function signup(){
 function hover(){
 
 };
+
+
