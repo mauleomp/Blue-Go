243263c1fdc203b4d079b9be575c48bcdf61e17c
s2356546 <f.gonzalez-novolopez@student.utwente.nl>
2022-02-23 14:27:57 +0100

    Passwords need to match

--- static/SignUp.js
+++ static/SignUp.js
@@ -7,23 +7,6 @@ document.getElementById("login").addEventListener("click", function() {
     window.location.href = 'Login.html';
 });
 
-/*window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-</script>*/
-
 function show_password() {
   var x = document.getElementById("pass");
   var show_eye = document.getElementById("show");
@@ -55,3 +38,43 @@ function show_password2() {
     hide_eye.style.display = "none";
   }
 }
+
+var check = function() {
+  if (document.getElementById('pass').value == document.getElementById('pass2').value) {
+      document.getElementById('pass2').classList.remove("is-invalid");
+
+  } else {
+    document.getElementById('pass2').classList.add("is-invalid");
+    document.getElementById('pass2').classList.remove("is-valid");
+    document.getElementById('message').innerHTML = 'Passwords are not matching';
+    document.getElementById('message').style.color = 'red';
+  }
+}
+
+// If any button has class is-invalid, disable the sign up button.
+// Make sure all elements are filled in. Else mark as in-valid
+// Make sure email address has '@' on it.
+
+/*
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+document.getElementById("submit").addEventListener("click", function() {
+    window.location.href = 'Login.html';
+});
+*/
