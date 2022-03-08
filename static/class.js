fd7bcc8d2b70a57d8dfe55f23c8e219873c65cdb
maury <m.l.merchanprado@student.utwente.nl>
2022-03-08 15:02:22 +0100

    fatima's and mau's

--- static/class.js
+++ static/class.js
@@ -2,3 +2,8 @@
 document.getElementById("icon").addEventListener("click", function() {
     window.location.href = 'TeacherSide.html';
 });
+
+$(document).on('click', '.delete', function (event) {
+    event.preventDefault();
+    $(this).closest('tr').remove();
+});
