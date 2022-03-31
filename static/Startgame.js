a7d97292f7078d1038f0e76837a10c779865be7b
Bas <bas.marcelis@gmail.com>
2022-03-31 11:09:27 +0200

    Added playing page, checkbuzzers page. Changes made to startgame

--- static/Startgame.js
+++ static/Startgame.js
@@ -3,18 +3,26 @@ var selectedGamemode;
 
 function selectgroup(id) {
   if (typeof selectedGroup !== "undefined") {
-    document.getElementById(selectedGroup).style.backgroundColor = "white";
+    document.getElementById(selectedGroup).style.backgroundColor = "rgba(255, 255, 255, 0.9)";
   }
   document.getElementById(id).style.backgroundColor = "orange";
   selectedGroup = id;
+  checkstart();
   return false;
 }
 
 function selectGamemode(id) {
     if (typeof selectedGamemode !== "undefined") {
-      document.getElementById(selectedGamemode).style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+      document.getElementById(selectedGamemode).style.backgroundColor = "rgba(255, 255, 255, 0.9)";
     }
     document.getElementById(id).style.backgroundColor = "orange";
     selectedGamemode = id;
+    checkstart();
     return false;
+}
+
+function checkstart() {
+  if (typeof selectedGroup !== "undefined" && typeof selectedGamemode !== "undefined") {
+    document.getElementById("startbutton").disabled = false;
+  }
 }
