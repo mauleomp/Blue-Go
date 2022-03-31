270b04ce5558d52d0e53a4ac5f1d90517f00bd64
s2356546 <f.gonzalez-novolopez@student.utwente.nl>
2022-03-31 22:31:30 +0200

    Changes

--- Server/static/class.js
+++ Server/static/class.js
@@ -8,5 +8,16 @@ $(document).on('click', '.delete', function (event) {
     $(this).closest('tr').remove();
 });
 
+const top = document.getElementById("top");
 
-const element = document.getElementById("id01");
+const group = document.getElementById("group");
+
+var isExpanded = document.getElementById("fati").attr("aria-expanded");
+
+if (isExpanded == true) {
+   top.style.color = "#ff0000";
+};
+
+if (isExpanded == false) {
+   top.style.color = "#ff0000";
+};
