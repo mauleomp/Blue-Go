70858722851734127e0508b88da7900222ec226d
s2356546 <f.gonzalez-novolopez@student.utwente.nl>
2022-04-11 10:49:07 +0200

    Managed to make notifications work, still need to understand them and be able to display a picture

--- Server/static/MainPage.js
+++ Server/static/MainPage.js
@@ -11,4 +11,51 @@ function hover(){
 
 };
 
+function button_click() {
+// Notification
+    (async () => {
+        // create and show the notification
+        const showNotification = () => {
+            // create a new notification
+            const notification = new Notification('Blue&GO!', {
+                //The body is the text that will go inside the notification
+                icon: "{{ url_for('images', filename='logo.png') }}",
+                body: 'Thank you for registering for Blue&GO'
+            });
+
+            // close the notification after 10 seconds
+            setTimeout(() => {
+                notification.close();
+            }, 10 * 1000);
+
+            // navigate to a URL when clicked
+            notification.addEventListener('click', () => {
+
+                window.open('http://127.0.0.1:5000/', '_blank');
+            });
+        }
+
+        // show an error message
+        const showError = () => {
+            const error = document.querySelector('.error');
+            error.style.display = 'block';
+            error.textContent = 'You blocked the notifications';
+        }
+
+        // check notification permission
+        let granted = false;
+
+        if (Notification.permission === 'granted') {
+            granted = true;
+        } else if (Notification.permission !== 'denied') {
+            let permission = await Notification.requestPermission();
+            granted = permission === 'granted' ? true : false;
+        }
+
+        // show notification or error
+        granted ? showNotification() : showError();
+
+    })();
+
+}
 
