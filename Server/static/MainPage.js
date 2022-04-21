1330abc65c87c8eb97cc879c6fee33ec8c6982ad
bryan <b.m.sanchezharo@gmail.com>
2022-04-21 13:11:50 +0200

    DB connections to the server database

--- Server/static/MainPage.js
+++ Server/static/MainPage.js
@@ -10,3 +10,23 @@ function signup(){
 function hover(){
 
 };
+
+
+
+// Notification
+(async () => {
+
+    // check notification permission
+    let granted = false;
+
+    if (Notification.permission === 'granted') {
+        granted = true;
+    } else if (Notification.permission !== 'denied') {
+        let permission = await Notification.requestPermission();
+        granted = permission === 'granted' ? true : false;
+    }
+
+    // show notification or error
+    granted ? showNotification() : showError();
+
+})();
--- Server/static/leaderboard.js
+++ Server/static/leaderboard.js
@@ -196,6 +196,53 @@ function correctanswer() {
   }, 3000);
 }
 
+function signalNotification(text){
+  // Notification
+      (async () => {
+          // create and show the notification
+          const showNotification = () => {
+              // create a new notification
+              const notification = new Notification('Blue&GO!', {
+                  //The body is the text that will go inside the notification
+                  icon: "/images/logo",
+                  body: text
+              });
+
+              // close the notification after 10 seconds
+              setTimeout(() => {
+                  notification.close();
+              }, 10 * 1000);
+
+              // navigate to a URL when clicked
+              notification.addEventListener('click', () => {
+
+                  window.parent.parent.focus();
+              });
+          }
+
+          // show an error message
+          const showError = () => {
+              const error = document.querySelector('.error');
+              error.style.display = 'block';
+              error.textContent = 'You blocked the notifications';
+          }
+
+          // check notification permission
+          let granted = false;
+
+          if (Notification.permission === 'granted') {
+              granted = true;
+          } else if (Notification.permission !== 'denied') {
+              let permission = await Notification.requestPermission();
+              granted = permission === 'granted' ? true : false;
+          }
+
+          // show notification or error
+          granted ? showNotification() : showError();
+
+      })();
+}
+
 
 function getIsCorrect() {
     return new Promise(function(resolve, reject) {
